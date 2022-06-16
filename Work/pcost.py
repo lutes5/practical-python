eac4a425a4afb64f33b54bb7ab93ebc84bff12e1
lutes5 <107516337+lutes5@users.noreply.github.com>
2022-06-16 11:43:01 -0700

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+totalcost = 0.0
+with open('Downloads/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2])
+        totalcost += shares * price
+        
+print('Total cost: ', totalcost)
