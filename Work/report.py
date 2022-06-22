a3495b8d78f9336d42ada04f6bc342d991cfb662
lutes5 <107516337+lutes5@users.noreply.github.com>
2022-06-22 16:14:31 -0700

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -20,15 +20,15 @@ import csv
 
 def read_portfolio(filename):
     '''Creates dictionary of a portfolio file'''
-    portfolio = {}
+    portfolio = []
     
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
             stock = {
-                'name': row[0]
-                'shares': int(row[1])
+                'name': row[0],
+                'shares': int(row[1]),
                 'price': float(row[2]))
             }
             portfolio.append(stock)
