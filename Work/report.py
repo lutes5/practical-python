c45c6973dfecefc832d112dd467e4fd2f9d472ba
lutes5 <107516337+lutes5@users.noreply.github.com>
2022-06-22 15:18:07 -0700

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -14,3 +14,39 @@ def read_portfolio(filename):
             holding = (row[0], int(row[1]), float(row[2]))
             portfolio.append(holding)
     return portfolio
+
+# Exercise 2.5
+import csv
+
+def read_portfolio(filename):
+    '''Creates dictionary of a portfolio file'''
+    portfolio = {}
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            stock = {
+                'name': row[0]
+                'shares': int(row[1])
+                'price': float(row[2]))
+            }
+            portfolio.append(stock)
+    return portfolio
+
+# Exercise 2.6 
+import csv
+
+def read_prices(filename):
+    '''Reads stock prices and put into a dictionary'''
+    stock_prices = {}
+    
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                stock_prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return stock_prices
+                
