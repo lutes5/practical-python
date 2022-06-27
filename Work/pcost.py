bb1d215abda2049ac7dc289e69e995f0226b51de
lutes5 <107516337+lutes5@users.noreply.github.com>
2022-06-27 16:47:08 -0700

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -11,3 +11,31 @@ with open('Downloads/portfolio.csv', 'rt') as f:
         totalcost += shares * price
         
     print('Total cost', totalcost)
+    
+# Exercise 2.15
+import csv
+def portfolio_cost(filename):
+    totalcost = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+                totalcost += shares * price
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+    return total_cost
+        
+print('Total cost', totalcost)
+
+cost = portfolio_cost('Downloads/missing.csv')
+
+
+
+
+
+***
+pandas
+cborne.plot
